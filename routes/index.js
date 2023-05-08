0e4ab81aaa76ec993f7cf5c9d340f00b79985daa
DyotisianAnurag <anurag@dyotis.com>
2023-05-08 13:07:22 +0530

    Debugging

--- routes/index.js
+++ routes/index.js
@@ -1,33 +1,39 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const passport = require('passport');
-const { generateJWT } = require('../middlewares/generateJWT');
+const passport = require("passport");
+const { generateJWT } = require("../middlewares/generateJWT");
 
 router.get(
-  '/google',
-  passport.authenticate('google', {
-    scope: ['profile', 'email'],
+  "/google",
+  passport.authenticate("google", {
+    scope: ["profile", "email"],
   }),
 );
 
 const clientUrl = process.env.CLIENT_URL_DEV;
 
 router.get(
-  '/google/callback',
-  passport.authenticate('google', {
-    failureRedirect: '/',
+  "/google/callback",
+  passport.authenticate("google", {
+    failureRedirect: "/",
     session: false,
   }),
   (req, res) => {
     const token = generateJWT(req.user);
-    res.cookie('x-auth-cookie', token);
+    res.cookie("x-auth-cookie", token, {
+      domain: "https://oauthclient.onrender.com",
+      expires: new Date(Date.now() + 900000),
+      httpOnly: false,
+      secure: true,
+      sameSite: "none",
+    });
     res.redirect(clientUrl);
   },
 );
 
-router.get('/logout', (req, res) => {
+router.get("/logout", (req, res) => {
   req.logout();
-  res.status(200).json({ message: 'Logged out successfully.' });
+  res.status(200).json({ message: "Logged out successfully." });
 });
 
 module.exports = router;
