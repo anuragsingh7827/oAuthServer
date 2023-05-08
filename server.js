cd8551bc9d538e290af12c5dd7b6e07edbdbbdc6
DyotisianAnurag <anurag@dyotis.com>
2023-05-08 11:19:30 +0530

    Scripts updated and applied checks for database URL

--- server.js
+++ server.js
@@ -1,24 +1,25 @@
-require('dotenv').config();
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
 const app = express();
-const mongoose = require('mongoose');
-const passport = require('passport');
-const cors = require('cors');
-const authRoutes = require('./routes');
+const mongoose = require("mongoose");
+const passport = require("passport");
+const cors = require("cors");
+const authRoutes = require("./routes");
 
 app.use(cors());
 
 app.use(passport.initialize());
-require('./services/googleStrategy');
+require("./services/googleStrategy");
 
-const dbConnection = process.env.MONGO_URI_DEV;
+const dbConnection =
+  process.env.MONGO_URI_DEV || "mongodb://127.0.0.1:27017/mernboilerplate";
 
 mongoose
   .connect(dbConnection)
-  .then(() => console.log('DB Connected...'))
+  .then(() => console.log("DB Connected..."))
   .catch((err) => console.log(err));
 
-app.use('/auth', authRoutes);
+app.use("/auth", authRoutes);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server running on port ${port}`));
